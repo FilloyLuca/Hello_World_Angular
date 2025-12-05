d83160db252337e84c73ae2ce568cf525493ccd2
FilloyLuca <filloylucapro@gmail.com>
2025-12-05 15:50:13 +0100

    Affichage de l'heure et de la date en temps reel

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,16 +12,22 @@ export class App {
 
   constructor(private ipService:IpService){
     this.getIP()
+    this.getDate()
   }
 
   // title: string = 'projet titre';
   protected readonly title = signal('projet1')
   ipAddress: string = 'Inconnue IP';
+  date: string = 'Date Inconnue'
 
   getIP()
   {
     this.ipService.getIPAddress().subscribe((res:any)=>{
       this.ipAddress=res.ip;
     });
+  }
+
+  getDate(){
+    this.date = new Date().toLocaleString()
   }
 }
